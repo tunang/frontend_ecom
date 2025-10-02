e84ecbc91cc24d280f4fb38a102d780a694c65e2
anhnpt <nguyentuan20042207@gmail.com>
2025-10-02 17:52:02 +0700

    Feat: Landing page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,12 @@
 import Banner from "./Banner";
+import FeaturedBooks from "./FeaturedBooks";
 
 const HomePage = () => {
   return (
     <div className="w-full mt-6">
       <Banner />
 
-
-      <div className="flex flex-col items-center justify-center mt-10">
-        <h1 className="text-2xl font-bold text-amber-600   ">
-          Sách đáng chú ý
-        </h1>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <div className="bg-amber-100 p-4 rounded-lg">
-            <h2 className="text-lg font-bold">Sách 1</h2>
-          </div>
-        </div>
-      </div>
+      <FeaturedBooks />
     </div>
   );
 };
