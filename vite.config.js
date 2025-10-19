3f692aaf1271948cbf944724c3c121d92f54ec00
TunAng <nguyentuan20042207@gmail.com>
2025-10-20 01:46:19 +0700

    feat

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: true,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
